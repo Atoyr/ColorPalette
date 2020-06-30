b372cc6b010646ee8064d205a10bd3e5cd91f6ba
atoyr <lessironglance@gmail.com>
2020-06-30 23:27:51 +0900

    apply scss

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,37 @@
 <template>
   <div class="main">
-    <div class="color-circle">
+    <div class="py-12">
       <ColorCircle ref="colorCircle"></ColorCircle>
     </div>
-    <div class="color-slider">
-      <div>
-        <div class="slider-wrapper">
-          <p class="slider-text">S</p>
-          <input 
-             class="slider"
-             type="range" value="1" min="0" max="1" step="0.01" v-model="saturation"></input>
-          <p class="slider-text">{{vsaturation}}</p>
-        </div>
-        <div class="slider-wrapper">
-          <p class="slider-text">V</p>
-          <input 
-             class="slider"
-             type="range" value="1" min="0" max="1" step="0.01" v-model="valueBrightness"></input>
-          <p class="slider-text">{{vvalueBrightness}}</p>
-        </div>
-      </div>
+    <div class="slider-wrapper">
+      <p class="slider-text">S</p>
+      <input 
+         class="slider"
+         type="range" value="1" min="0" max="1" step="0.01" v-model="saturation"></input>
+      <p class="slider-text">{{vsaturation}}</p>
+    </div>
+    <div class="slider-wrapper">
+      <p class="slider-text">V</p>
+      <input 
+         class="slider py-4"
+         type="range" value="1" min="0" max="1" step="0.01" v-model="valueBrightness"></input>
+      <p class="slider-text">{{vvalueBrightness}}</p>
     </div>
     <div>
       <p>HSV : {{selectColor[0]}} {{selectColor[1] * 100}} {{selectColor[2] * 100}}</p>
       <p>RGB : {{selectRGBColor[0]}} {{selectRGBColor[1]}} {{selectRGBColor[2]}}</p>
     </div>
-    <div class="color-div">
+    <div class="slider-wrapper select-color-view py-2">
+      <div class="select-color" :style="selectColorSytle"></div>
+      <P>{{selectColorHex}}</p>
+    </div>
+    <div class="color-div py-2">
       <colorBox v-for="n of 8" :color="selectRGBColor" :borderColor="[32 * (n - 1),32 * (n - 1),32 * (n - 1)]"></colorBox>
     </div>
-    <div class="color-div">
+    <div class="color-div py-2">
       <colorBox v-for="n of 8" :borderColor="selectRGBColor" :color="[32 * (n - 1),32 * (n - 1),32 * (n - 1)]"></colorBox>
     </div>
+    <div class="circle" :style="circleStyle"></div>
   </div>
 </template>
 
@@ -39,7 +40,7 @@ import ColorCircle from '@/components/ColorCircle.vue'
 import colorBox from '@/components/box.vue'
 
 function toPaddedHexString(num, len) {
-    let str = num.toString(16);
+    let str = num.toString(16).toUpperCase();
     return "0".repeat(len - str.length) + str;
 }
 
@@ -83,6 +84,11 @@ export default {
         return this.$hsv2rgb(this.selectColor);
       }
     },
+    selectColorHex: {
+      get() {
+        return `#${toPaddedHexString(this.selectRGBColor[0],2)}${toPaddedHexString(this.selectRGBColor[1],2)}${toPaddedHexString(this.selectRGBColor[2],2)}`
+      }
+    },
     vsaturation: function(){
       return parseInt(this.saturation * 100)
     },
@@ -91,7 +97,13 @@ export default {
     },
     selectColorSytle() {
       return {
-        '--bg': `#${toPaddedHexString(this.selectRGBColor[0],2)}${toPaddedHexString(this.selectRGBColor[1],2)}${toPaddedHexString(this.selectRGBColor[2],2)}`
+        '--bg': this.selectColorHex
+      }
+    },
+    circleStyle() {
+      return {
+        '--bg-first': this.selectColorHex,
+        '--bg-second': "#FFFFFF"
       }
     }
 
@@ -103,14 +115,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .main {
   display: flex;
   flex-direction: column; 
   align-items: center;
-}
-.color-circle {
-  padding: 12px;
 }
 .color-slider {
   display: flex;
@@ -124,7 +133,6 @@ export default {
 }
 .slider {
   width: 200px;
-  padding: 4px;
 }
 .slider-text {
   width: 32px;
@@ -135,12 +143,29 @@ export default {
   align-items: center;
 }
 .select-color {
-  {
-    --bg: #FFFFFF;
-  }
+  --bg: #FFFFFF;
   background-color: var(--bg);
+  width: 90px;
+  height: 32px;
   display: flex;
   justify-content: center;
   align-items: center;
+}
+.circle {
+  --width: 32px;
+  --height: 32px;
+  --bg-first: #FFFFFF;
+  --bg-second: #000000;
+  --bg-gradient: linear-gradient(var(--bg-first),var(--bg-second));
+  background-image: var(--bg-gradient);
+  width: var(--width);
+  height: var(--height);
+  border-radius: 50%;
+}
+.select-color-view {
+  --width: 360px;
+  --height: 32px;
+  width: var(--width);
+  height: var(--height);
 }
 </style>
